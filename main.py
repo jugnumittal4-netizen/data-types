2a4ef54d0312f691a1baf0f2e09e45fb7434c8f8
jugnumittal4-netizen <jugnumittal4@gmail.com>
2026-06-27 13:49:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+a = "penguin"
+b = 15
+c = True
+d = 38.5
+print("a:",a)
+print("data type of a is",type(a))
+print("b:",b)
+print("data type of b is",type(b))
+print("c:",c)
+print("data type of c is",type(c))
+print("d:",d)
+print("data type of d is:",type(d))
+print("/n after type casting:")
+a=str(a)
+d = int(d)
+print("data tyle of a is",type(a))
+print("data type of d is",type(d))
